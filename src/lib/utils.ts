ec0c6dabc4fa5ace816a91a1b5e7187aed6a7bc9
Ashutosh Sharma <ashutoshsharma1395@gmail.com>
2026-05-04 16:21:29 +0530

    style: document the cn utility helper

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,11 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+/**
+ * Merge Tailwind utility classes and deduplicate conflicting values.
+ *
+ * This helper is used throughout the app for cleaner conditional class names.
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
